c936e6aa21529d0036f76ed25261d9465da6d999
ArthurGolubev <gg.arthur.go@gmail.com>
2023-09-26 16:49:03 +0700

    fix: Пробую тестировать backend на GitHub Actions, попытка номер 2

--- server/test_main.py
+++ server/test_main.py
@@ -1,12 +1,12 @@
 from fastapi.testclient import TestClient
 from loguru import logger
-from . import main
+from .main import app
 # 
 
 def test_read_main(monkeypatch):
     # monkeypatch.setattr("ee.Initialize", lambda _: logger.success('ee init'))
 
-    client = TestClient(main.app)
+    client = TestClient(app)
 
     response = client.get('/api/v2-rest')
     logger.debug(response)
